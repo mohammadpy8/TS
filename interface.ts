82d82e9084b124e650527f3b3a1951d610816be6
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-17 17:20:37 +0330

    implements

--- interface.ts
+++ interface.ts
@@ -32,3 +32,27 @@ pegeot206 = {
 
 pegeot206.driven(140);
 pegeot206.stop();
+
+class PegeotPars implements Car{
+    constructor(
+        public company: string,
+        public color: string,
+        public year: number,
+        public model: string,
+    ) {
+        this.company = company;
+        this.color = color;
+        this.year = year;
+        this.model = model;
+    };
+
+    driven(speed: number): void {
+        console.log(`${this.model} drive ${speed}Km`);
+    }
+
+    stop(): void {
+        console.log(`${this.color}`);
+    }
+}
+
+const Pars = new PegeotPars("", "", 12, "");
