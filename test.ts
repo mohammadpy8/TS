0512d66fe53111a433559c4bb838ccfb40629e3c
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-17 20:33:02 +0330

    type-gurd

--- test.ts
+++ test.ts
@@ -25,3 +25,29 @@ type Type1 = string | number;
 type Type2 = number | boolean;
 
 const testVar10: Type1 & Type2 = 10;
+
+//////////////////////////
+
+const showProp = (member: Admin | TeacherNew) => {
+
+    if ("statrDate" in member) {
+        console.log(member.statrDate);
+    } else if ("privolage" in member) {
+        console.log(member.privolage);   
+    }
+
+    console.log(member.name);
+};
+
+const rezaaaa: TeacherNew = {
+    name: "reza",
+    statrDate: new Date(),
+};
+
+const amin: Admin = {
+    name: "ggg",
+    privolage: [""]
+}
+
+showProp(rezaaaa);
+showProp(amin);
