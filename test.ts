73e2fc23beca3aa5c67475dd2bb36309d32f5afd
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-17 20:42:00 +0330

    gurd1

--- test.ts
+++ test.ts
@@ -51,3 +51,18 @@ const amin: Admin = {
 
 showProp(rezaaaa);
 showProp(amin);
+
+/////////////////////////////////
+
+type Combine = string | number;
+
+const add = (param1: Combine, param2: Combine): Combine => {
+    
+    if (typeof param1 === "string" && typeof param2 === "string") {
+        return param1.toString() + param2.toString();
+    } else if (typeof param1 === "number" && typeof param2 === "number") {
+        return param1 + param2;
+    }
+};
+
+    
