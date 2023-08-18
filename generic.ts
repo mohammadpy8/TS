fc674d559b27d7511f94810008b97f5ee52fffa7
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-18 08:23:14 +0330

    overLoadig

--- generic.ts
+++ generic.ts
@@ -13,9 +13,9 @@ type Teacher2 = {
 const showUsers = (member: Admin1 | Teacher2) => {
     switch (member.type) {
         case "ADMIN": {
-            console.log(member.privily);  
+            console.log(member.privily);
             break;
-        } 
+        }
         case "TEACHER": {
             console.log(member.startDate);
             break;
@@ -24,4 +24,25 @@ const showUsers = (member: Admin1 | Teacher2) => {
             throw new Error("invalid member");
         }
     }
-}
+};
+
+///////////////////////////////////////
+
+type param = string | number;
+
+function addto(param1: number, param2: number): number;
+function addto(param1: string, param2: string): string;
+
+function addto  (param1: param, param2: param) {
+    if (typeof param1 === "number" && typeof param2 === "number") {
+        return param1 + param2;
+    } else if (typeof param1 === "string" && typeof param2 === "string") {
+        return param1.toString() + param2.toString();
+    }
+};
+
+const reesult = addto("mohammad", "amin") as string;
+
+reesult.split("");
+
+console.log(reesult);
