09547129b9d86a324c7abefaf1e45128004c9b57
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-22 10:49:20 +0330

    extends-gneric-type

--- newApp.ts
+++ newApp.ts
@@ -33,13 +33,42 @@ console.log(finish);
 const finish10: string = echo<string>("ts");
 console.log(finish10);
 
-function merge<T, U>(obj1: T, obj2: U): T & U {
-    return { ...obj1, ...obj2 };
+function merge<T extends object, U extends object>(obj1: T, obj2: U): T & U {
+    // return { ...obj1, ...obj2 };
+    return Object.assign(obj1, obj2);
 };
 
-const mergonj = merge({ name: "ali" }, { age: 23 });
+const mergonj = merge({ name: "ali" }, {age: 12});
 console.log(mergonj.age);
 console.log(mergonj.name);
+
+///////////////////////////
+
+function myFunc10<T extends string>(param: T): T {
+    console.log(param.length)
+    return param;
+};
+
+function echoTest1<T>(param: T[]): T {
+
+    console.log(param.length);
+    
+    return param[0];
+};
+
+console.log(echoTest1([10, 12, 20]));
+
+interface Width {
+    length: number;
+}
+
+function echoTest2<T extends Width>(param : T) {
+    
+    console.log(param.length);
+
+};
+
+
 
 
 
