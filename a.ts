5b5beed2373da04c7e7b0191769743bfb676e634
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-22 16:06:15 +0330

    type-mapping

--- a.ts
+++ a.ts
@@ -28,3 +28,36 @@ class ListItems2 {
     }
 };
 
+//////////////////////////////////
+
+interface User {
+    username: string;
+    email: string;
+    age:number
+}
+
+interface optionalUser {
+    username?: string;
+    email?: string;
+}
+
+const alireza10: User = { username: "ali", email: "", age:10 };
+const mohammadreza10: optionalUser = {}; 
+
+type optialUserName<T> = {
+    [key in keyof T]?: T[key]
+};
+
+const alimohmmad: optialUserName<User> = { username: "" };  
+
+type readOnly = {
+    readonly [key in keyof User]: User[key];
+};
+
+const amialiii: readOnly = { username: "amir", email: "", age: 15 };
+amialiii.age = 25; ////error
+console.log(amialiii);
+
+
+
+
