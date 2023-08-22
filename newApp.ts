c8a00e37e96f344fa55e68c278019cb088858bb9
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-22 08:59:38 +0330

    genric-type

--- newApp.ts
+++ newApp.ts
@@ -9,6 +9,41 @@ console.log(isLoginForm({
 const usenamemmm = number15;
 console.log(usenamemmm);
 
-console.log(AddNew(12,20));
+console.log(AddNew(12, 20));
+
+/////////////////////////////
+function ecoStr (param : string){
+    return param
+}
+function ecoNumber (param : number){
+    return param
+}
+
+function echo<Type>(param: Type): Type {
+    return param
+}
+
+
+const result15: string = ecoStr("js");
+console.log(result15.length);
+
+const finish: number = echo<number>(12);
+console.log(finish);
+
+const finish10: string = echo<string>("ts");
+console.log(finish10);
+
+function merge<T, U>(obj1: T, obj2: U): T & U {
+    return { ...obj1, ...obj2 };
+};
+
+const mergonj = merge({ name: "ali" }, { age: 23 });
+console.log(mergonj.age);
+console.log(mergonj.name);
+
+
+
+
+
 
 
